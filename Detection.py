29aa4b5b36276dcb76c25c487df4c8b273cd7b49
adamoudad <adam.oudad@gmail.com>
2016-05-24 00:41:00 +0900

    Added Detector class

--- Detection.py
+++ Detection.py
@@ -13,3 +13,14 @@ import cv2
 def sift(image):
     sift = cv2.xfeatures2d.SIFT_create()
     return sift.detectAndCompute(image,None)
+
+class Detector:
+    def __init__(self,method="SIFT"):
+        self.method = method
+
+        def detect(self,image):
+            self.__getattr__(self.method)(image)
+
+        def SIFT(self,image):
+            sift = cv2.xfeatures2d.SIFT_create()
+            return sift.detectAndCompute(image,None)
